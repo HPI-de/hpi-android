48125cc294c9b9d97dd2ef249543e73defeb0a6f
Jonas Wanke <contact@jonas-wanke.com>
2019-01-31 00:17:32 +0100

    build(core): add core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,23 +1,23 @@
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 
-plugins {
-    id("com.android.application")
 
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    id("kotlin-kapt")
+plugins {
+    id("com.android.library")
 }
 
 android {
     compileSdkVersion(28)
+
     defaultConfig {
-        applicationId = "de.hpi.android"
         minSdkVersion(19)
         targetSdkVersion(28)
         versionCode = 2
         versionName = "0.2.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
@@ -27,13 +27,10 @@ android {
 }
 
 dependencies {
-    implementation(project(":core"))
-
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
 
     implementation("androidx.appcompat:appcompat:1.0.2")
     implementation("androidx.core:core-ktx:1.1.0-alpha04")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test:runner:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
