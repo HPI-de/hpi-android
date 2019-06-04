21754dfdad42c8c9725a4f496e09fa507a27cba8
Marcel Garus <marcel.garus@gmail.com>
2019-06-04 21:00:18 +0200

    fix(menu): label uses Id instead of String

--- menu/src/main/java/de/hpi/android/menu/data/LabelDto.kt
+++ menu/src/main/java/de/hpi/android/menu/data/LabelDto.kt
@@ -1,8 +1,9 @@
 package de.hpi.android.menu.data
 
 import de.hpi.android.core.data.Dto
+import de.hpi.android.core.data.Id
 
 data class LabelDto(
-    override val id: String,
+    override val id: Id<LabelDto>,
     val title: String
 ): Dto<LabelDto>
