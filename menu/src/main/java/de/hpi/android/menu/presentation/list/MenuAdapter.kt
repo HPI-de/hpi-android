c191cd6cc5830db8746350fd1d7e19baaf25f972
Marcel Garus <marcel.garus@gmail.com>
2019-06-11 22:36:34 +0200

    feat(menu): add labels

--- menu/src/main/java/de/hpi/android/menu/presentation/list/MenuAdapter.kt
+++ menu/src/main/java/de/hpi/android/menu/presentation/list/MenuAdapter.kt
@@ -2,6 +2,7 @@ package de.hpi.android.menu.presentation.list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.google.android.material.chip.Chip
 import de.hpi.android.core.presentation.base.BaseAdapter
 import de.hpi.android.core.presentation.base.BaseViewHolder
 import de.hpi.android.menu.databinding.ItemMenuBinding
@@ -21,6 +22,15 @@ class MenuAdapter : BaseAdapter<Menu, MenuAdapter.ViewHolder, ItemMenuBinding>()
     class ViewHolder(binding: ItemMenuBinding) : BaseViewHolder<Menu, ItemMenuBinding>(binding) {
         override fun onItemSet() {
             binding.menu = item
+            with (binding.labels) {
+                removeAllViews()
+                for (label in item.labels) {
+                    val chip = Chip(context)
+                    chip.chipIcon = context.getDrawable(android.R.drawable.ic_delete)
+                    chip.shapeAppearanceModel =
+                    addView(chip)
+                }
+            }
         }
     }
 }
