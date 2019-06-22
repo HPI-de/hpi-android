e74abb2bcad3c610495e082ea61394383eed118b
Maximilian Stiede <maximilian.stiede@student.hpi.uni-potsdam.de>
2019-06-22 18:34:10 +0200

    feat(feedback): add screenshot saving (broken)

--- feedback/src/main/java/de/hpi/android/feedback/presentation/FeedbackDialogFragment.kt
+++ feedback/src/main/java/de/hpi/android/feedback/presentation/FeedbackDialogFragment.kt
@@ -2,15 +2,23 @@ package de.hpi.android.feedback.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
 import de.hpi.android.core.presentation.base.BaseBottomSheepDialogFragment
 import de.hpi.android.feedback.databinding.DialogFeedbackBinding
+import android.graphics.Bitmap
+import android.R.layout
+import de.hpi.android.feedback.presentation.utils.createScreenshot
+import java.net.URI
+
 
 class FeedbackDialogFragment : BaseBottomSheepDialogFragment<DialogFeedbackBinding, FeedbackViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(FeedbackViewModel::class.java)
+        viewModel.referringScreen = URI.create(activity?.toString())
+        viewModel.screenshot = activity?.window?.decorView?.rootView?.createScreenshot()
     }
 
     override fun onCreateBinding(
--- feedback/src/main/java/de/hpi/android/feedback/presentation/FeedbackViewModel.kt
+++ feedback/src/main/java/de/hpi/android/feedback/presentation/FeedbackViewModel.kt
@@ -1,18 +1,25 @@
 package de.hpi.android.feedback.presentation
 
+import android.graphics.Bitmap
 import de.hpi.android.core.presentation.base.BaseViewModel
 import de.hpi.android.feedback.data.FeedbackDto
 import de.hpi.android.feedback.data.FeedbackRepository
+import de.hpi.android.feedback.presentation.utils.asTemporaryFile
 import java.net.URI
 
 class FeedbackViewModel : BaseViewModel() {
+
+    var referringScreen: URI = URI.create("/")
+    var screenshot: Bitmap? = null
+
     fun onSend(message: String, includeUser: Boolean, includeDebugData: Boolean) {
         val feedback = FeedbackDto(
-            message = "asdf",
+            message = message,
             meta = FeedbackDto.Metadata(
-                screenUri = URI.create("/"),
-                author = "DAU",
-                log = "404 log not found"
+                screenUri = referringScreen,
+                author = if (includeUser) "User#123" else "Anonymous User",
+                screenshot = if (includeDebugData) screenshot?.asTemporaryFile() else null,
+                log = if (includeDebugData) "log log log" else null
             )
         )
         FeedbackRepository.create(feedback)
