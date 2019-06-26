3665a909d58e075bff73c3a82b47f2e3ab8c31ed
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2019-06-26 16:58:35 +0200

    add course to view model of courseDetail

--- course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailViewModel.kt
+++ course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailViewModel.kt
@@ -4,9 +4,12 @@ import de.hpi.android.core.presentation.base.BaseViewModel
 import de.hpi.android.core.utils.asLiveData
 import de.hpi.android.core.utils.data
 import de.hpi.android.course.domain.GetCourseDetailUseCase
+import de.hpi.android.course.domain.GetCourseUseCase
 
 class CourseDetailViewModel : BaseViewModel() {
-    private val courseDetailId = "2019ss-www"
-    private val courseDetailResult = GetCourseDetailUseCase(courseDetailId).asLiveData()
+    private val courseId = "2019ss-www"
+    private val courseDetailResult = GetCourseDetailUseCase(courseId).asLiveData()
+    private val courseResult = GetCourseUseCase(courseId).asLiveData()      // get this from the parent activity
     val courseDetail = courseDetailResult.data
+    val course = courseResult.data
 }
