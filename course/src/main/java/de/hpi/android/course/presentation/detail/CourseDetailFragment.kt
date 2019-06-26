636a1968b8ad2b1a8e364b6d2e85d6054acc8f52
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2019-06-26 16:57:27 +0200

    add methods to the binding
    method to open tele-TASK link
    method to change the visibility of text views

--- course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailFragment.kt
+++ course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailFragment.kt
@@ -1,14 +1,18 @@
 package de.hpi.android.course.presentation.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import de.hpi.android.core.presentation.base.BaseFragment
 import de.hpi.android.course.R
 import de.hpi.android.course.databinding.FragmentCourseDetailBinding
+import kotlinx.android.synthetic.main.fragment_course_detail.*
 
 class CourseDetailFragment : BaseFragment<FragmentCourseDetailBinding, CourseDetailViewModel>() {
 
@@ -24,6 +28,28 @@ class CourseDetailFragment : BaseFragment<FragmentCourseDetailBinding, CourseDet
     ): FragmentCourseDetailBinding {
         return FragmentCourseDetailBinding.inflate(inflater, container, false).also {
             it.viewModel = viewModel
+            it.onTeleTaskClicked = {
+                val uri = viewModel.courseDetail.value?.teleTask
+                if (uri != null) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString()))
+                    startActivity(intent)
+                }
+            }
+            it.onToggleView = { i ->
+                val view = when (i) {
+                    0 -> tv_description
+                    1 -> tv_requirements
+                    2 -> tv_learning
+                    3 -> tv_examination
+                    4 -> tv_dates
+                    else -> tv_literature   // 5
+                }
+                if (view.isVisible) {
+                    view.visibility = View.GONE
+                } else {
+                    view.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
