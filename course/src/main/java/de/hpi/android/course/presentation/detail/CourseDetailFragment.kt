6053f21574e3e059da59319f305e2aa10fc2c919
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2019-06-30 09:43:58 +0200

    ui(course): show content of complex TextViews

--- course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailFragment.kt
+++ course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailFragment.kt
@@ -4,21 +4,41 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import de.hpi.android.core.presentation.base.BaseFragment
 import de.hpi.android.course.R
 import de.hpi.android.course.databinding.FragmentCourseDetailBinding
-import kotlinx.android.synthetic.main.fragment_course_detail.*
+import de.hpi.android.course.domain.Course
 
 class CourseDetailFragment : BaseFragment<FragmentCourseDetailBinding, CourseDetailViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CourseDetailViewModel::class.java)
+
+        viewModel.course.observe(this, Observer { course ->
+            viewModel.generalInformation.value = getString(
+                R.string.course_courseDetails_details,
+                getString(
+                    if (course?.series?.mandatory == true) R.string.course_courseSeries_mandatory_true
+                    else R.string.course_courseSeries_mandatory_false
+                ),
+                course?.series?.ects,
+                course?.series?.hoursPerWeek,
+                course?.type?.joinToString(separator = getString(R.string.course_courseSeries_type_seperator)) {
+                    getString(
+                        when (it) {
+                            Course.Type.LECTURE -> R.string.course_courseSeries_type_lecture
+                            Course.Type.BLOCK_SEMINAR -> R.string.course_courseSeries_type_blockSeminar
+                            Course.Type.EXERCISE -> R.string.course_courseSeries_type_exercise
+                            Course.Type.SEMINAR -> R.string.course_courseSeries_type_seminar
+                        }
+                    )
+                }
+            )
+        })
     }
 
     override fun onCreateBinding(
@@ -36,14 +56,5 @@ class CourseDetailFragment : BaseFragment<FragmentCourseDetailBinding, CourseDet
                 }
             }
         }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        baseActivity.setSupportActionBar(view.findViewById(R.id.toolbar))
-
-        viewModel.courseDetail.observe(this, Observer {
-            // update UI
-        })
     }
 }
--- course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailViewModel.kt
+++ course/src/main/java/de/hpi/android/course/presentation/detail/CourseDetailViewModel.kt
@@ -1,15 +1,33 @@
 package de.hpi.android.course.presentation.detail
 
+import androidx.lifecycle.MutableLiveData
 import de.hpi.android.core.presentation.base.BaseViewModel
 import de.hpi.android.core.utils.asLiveData
 import de.hpi.android.core.utils.data
+import de.hpi.android.core.utils.map
 import de.hpi.android.course.domain.GetCourseDetailUseCase
 import de.hpi.android.course.domain.GetCourseUseCase
 
 class CourseDetailViewModel : BaseViewModel() {
-    private val courseId = "2019ss-www"
+    private val courseId = "2019ss-www"     // still hardcoded
     private val courseDetailResult = GetCourseDetailUseCase(courseId).asLiveData()
-    private val courseResult = GetCourseUseCase(courseId).asLiveData()      // get this from the parent activity
+    private val courseResult = GetCourseUseCase(courseId).asLiveData()      // get this from the parent activity?
     val courseDetail = courseDetailResult.data
     val course = courseResult.data
+    val assistants = course.map { it?.assistants?.joinToString() }
+    val programsModules = courseDetail.map { courseDetail ->
+        val lines = mutableSetOf<String>()
+        courseDetail?.let {
+            for ((program, moduleList) in courseDetail.programs) {
+                lines.add(program)
+                for (module in moduleList) {
+                    lines.add("\t\t" + module)
+                }
+            }
+        }
+        lines.joinToString(separator = "\n")
+    }
+    val generalInformation: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
 }
